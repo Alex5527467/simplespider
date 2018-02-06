42830b8f9f70906febcd17f70ecc6a7b2d850643
Alex <xinchen296@163.com>
2018-02-06 08:21:04 +0800

    simple spider modify

--- spidertest/spiderfff.py
+++ spidertest/spiderfff.py
@@ -12,6 +12,10 @@ url1 = 'http://movie.douban.com/top250'
 pages = [0,25,50,75,100,125,150,175,200,225]
 allmovie = []
 allurl=[]
+allscore=[]
+allyear=[]
+allstyle=[]
+alldescribe=[]
 #import pdb
 #pdb.set_trace()
 
@@ -36,15 +40,22 @@ def spider_context(url):
 	info = re.findall('<ol class="grid_view">(.*?)</ol>',code,re.S)
 	mov_url = re.findall('<a href="(.*?)"',info[0],re.S)
 	movie = re.findall('alt="(.*?)" src',info[0],re.S)
+	score= re.findall('<span class="rating_num" property="v:average">(.*?)</span>',info[0],re.S)
+	year = re.findall('<br>(.*?)&nbsp',info[0],re.S)
+	style= re.findall('<p class="">(.*?)</p>',info[0],re.S)
+	describe=re.findall('<span class="inq">(.*?)</span>',info[0],re.S)
 	#star = re.findall('><em>(.*?)</em></span>',info[0],re.S)
-	return movie,mov_url
+	return movie,mov_url,score,year,style,describe
 
 for page in pages: 
     address = 'http://movie.douban.com/top250?start=' + str(page) + '&filter=&type='
-    movie,mov_url=spider_context(address)
+    movie,mov_url,score,year,style,describe=spider_context(address)
     allmovie.extend(movie)
     allurl.extend(mov_url)
-
+    allscore.extend(score)
+    allyear.extend(year)
+    allstyle.extend(style)
+    alldescribe.extend(describe)
 #i = 0 
 #for mov in movie:
 #    allmovie.append((mov,mov_url[i]))
@@ -52,8 +63,16 @@ for page in pages:
 
 fileObject = open('All_Movies.txt', 'w')
 for i in range(0,len(allmovie)):
-    fileObject.write(str(i)+' '+str(allmovie[i])+' '+str(allurl[i]))
-    fileObject.write('\n')
+    try:
+        age=int(allyear[i])
+        if age>=2010:
+            fileObject.write(str(i)+' '+str(allmovie[i])+' '+str(allurl[i])+' '+str(allscore[i])+' '+str(allyear[i])+' '+str(allstyle[i])+' '+str(alldescribe[i]))
+            fileObject.write('\n')
+    except:
+        print allyear[i]+"can't convert to int"
+        continue
+    
 
 fileObject.close()
 
+print "OK"
